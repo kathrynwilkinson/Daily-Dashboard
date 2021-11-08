de0bcc5b87ff0248514e4d3ed2f241f2bf984094
Michael O'Connor <oconnor97@aol.com>
2021-11-08 15:23:30 -0500

    editing innerHTML to incluse the time zone

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,23 +9,23 @@ let easternTime = setInterval(easternTimer, 1000);
 function pacificTimer() {
     let day = new Date();
     let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' });
-    document.getElementById("pacificTime").innerHTML = time;
+    document.getElementById("pacificTime").innerHTML = time + " PST";
 }
 
 function mountainTimer() {
     let day = new Date();
     let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Denver' });
-    document.getElementById("mountainTime").innerHTML = time;
+    document.getElementById("mountainTime").innerHTML = time + " MST";
 }
 
 function centralTimer() {
     let day = new Date();
     let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Chicago' });
-    document.getElementById("centralTime").innerHTML = time;
+    document.getElementById("centralTime").innerHTML = time + " CST";
 }
 
 function easternTimer() {
     let day = new Date();
     let time = day.toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
-    document.getElementById("easternTime").innerHTML = time;
+    document.getElementById("easternTime").innerHTML = time + " EST";
 }
