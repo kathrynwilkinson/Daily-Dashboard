45f5f478e9db7fa0fb1b2955efec51f5267cb319
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-11-09 02:57:40 -0600

    refactor: Add seperate divs for time-zone-identifiers + refactor js dom manipulation

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,23 +9,27 @@ let easternTime = setInterval(easternTimer, 1000);
 function pacificTimer() {
     let day = new Date();
     let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' });
-    document.getElementById("pacificTime").innerHTML = time + " PST";
+    document.getElementById( "pst-display" ).innerHTML = "PST";
+    document.getElementById( "pacificTime" ).innerHTML = time;
 }
 
 function mountainTimer() {
     let day = new Date();
-    let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Denver' });
-    document.getElementById("mountainTime").innerHTML = time + " MST";
+    let time = day.toLocaleTimeString( 'en-US', { timeZone: 'America/Denver' } );
+    document.getElementById( "mst-display" ).innerHTML = "MST";
+    document.getElementById("mountainTime").innerHTML = time;
 }
 
 function centralTimer() {
     let day = new Date();
-    let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Chicago' });
-    document.getElementById("centralTime").innerHTML = time + " CST";
+    let time = day.toLocaleTimeString( 'en-US', { timeZone: 'America/Chicago' } );
+    document.getElementById( "cst-display" ).innerHTML = "CST";
+    document.getElementById("centralTime").innerHTML = time;
 }
 
 function easternTimer() {
     let day = new Date();
-    let time = day.toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
-    document.getElementById("easternTime").innerHTML = time + " EST";
+    let time = day.toLocaleTimeString( 'en-US', { timeZone: 'America/New_York' } );
+    document.getElementById( "est-display" ).innerHTML = "EST";
+    document.getElementById("easternTime").innerHTML = time;
 }
