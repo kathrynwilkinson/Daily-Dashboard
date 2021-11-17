9e927269ef708c9a593eda7931af96f98e539e94
Jon Jackson <ocskier@gmail.com>
2021-11-17 15:45:50 -0500

    feat: adds ckeditors or calendars

---
+++ assets/JS/editors.js
@@ -0,0 +1,14 @@
+ClassicEditor.create(document.querySelector('#today-editor'))
+  .then((editor) => {
+    console.log(editor);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+ClassicEditor.create(document.querySelector('#tomorrow-editor'))
+  .then((editor) => {
+    console.log(editor);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
