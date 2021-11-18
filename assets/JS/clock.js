0d120db8ebf00b055ddfab6b9b76740104979d9d
Jon Jackson <ocskier@gmail.com>
2021-11-18 12:21:33 -0500

    chore: adds prettier config for consistent format

--- assets/JS/clock.js
+++ assets/JS/clock.js
@@ -1,9 +1,7 @@
 // Nav Menu functionality
-function displayMenu () {
-    const menu = document.getElementById("menu-icon")
+function displayMenu() {
+    const menu = document.getElementById('menu-icon');
 }
-
-
 
 // Updating the time every second
 let pacificTime = setInterval(pacificTimer, 1000);
@@ -11,32 +9,31 @@ let mountainTime = setInterval(mountainTimer, 1000);
 let centralTime = setInterval(centralTimer, 1000);
 let easternTime = setInterval(easternTimer, 1000);
 
-
 // Creating new times for each timeZone
 function pacificTimer() {
     let day = new Date();
     let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' });
-    document.getElementById( "pst-display" ).innerHTML = "PST";
-    document.getElementById( "pacificTime" ).innerHTML = time;
+    document.getElementById('pst-display').innerHTML = 'PST';
+    document.getElementById('pacificTime').innerHTML = time;
 }
 
 function mountainTimer() {
     let day = new Date();
-    let time = day.toLocaleTimeString( 'en-US', { timeZone: 'America/Denver' } );
-    document.getElementById( "mst-display" ).innerHTML = "MST";
-    document.getElementById("mountainTime").innerHTML = time;
+    let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Denver' });
+    document.getElementById('mst-display').innerHTML = 'MST';
+    document.getElementById('mountainTime').innerHTML = time;
 }
 
 function centralTimer() {
     let day = new Date();
-    let time = day.toLocaleTimeString( 'en-US', { timeZone: 'America/Chicago' } );
-    document.getElementById( "cst-display" ).innerHTML = "CST";
-    document.getElementById("centralTime").innerHTML = time;
+    let time = day.toLocaleTimeString('en-US', { timeZone: 'America/Chicago' });
+    document.getElementById('cst-display').innerHTML = 'CST';
+    document.getElementById('centralTime').innerHTML = time;
 }
 
 function easternTimer() {
     let day = new Date();
-    let time = day.toLocaleTimeString( 'en-US', { timeZone: 'America/New_York' } );
-    document.getElementById( "est-display" ).innerHTML = "EST";
-    document.getElementById("easternTime").innerHTML = time;
+    let time = day.toLocaleTimeString('en-US', { timeZone: 'America/New_York' });
+    document.getElementById('est-display').innerHTML = 'EST';
+    document.getElementById('easternTime').innerHTML = time;
 }
--- assets/JS/nameGenerator.js
+++ assets/JS/nameGenerator.js
@@ -1,4 +1,4 @@
-const names = ['Amina', 'Babajide', 'Kenneth', 'Wayeu', 'Myesha', 'Stteffanny', 'Valentina', 'Victoria', 'Derrick', 'Mesfin', 'Yonatan', 'Catherine', 'Alex Latte', 'Danielle', 'Anthony Lomax', 'Yohannes', 'Mathew Olajide', 'Slav', 'Hemalatha', 'Sri', 'Daniel', 'Saul', 'Santosh', 'Humaira', 'Alex Torres', 'Aboma', 'Helen', 'Synquis'];
+const names = ['Amina', 'Babajide', 'Kenneth', 'Wayeu', 'Myesha', 'Stteffanny', 'Valentina', 'Victoria', 'Derrick', 'Mesfin', 'Yonatan', 'Catherine', 'Alex Latte', 'Danielle', 'Anthony Lomax', 'Yohannes', 'Mathew Olajide', 'Slav', 'Hemalatha', 'Sri', 'Daniel', 'Saul', 'Santosh', 'Humaira', 'Alex Torres', 'Aboma', 'Helen', 'Synquis']; // prettier-ignore
 
 const getNameButton = document.getElementById('nameBtn');
 const clearButton = document.getElementById('clearBtn');
@@ -11,19 +11,20 @@ let myInterval;
 
 const clearBox = () => {
     namesList.innerHTML = ' ';
+    myInterval ? clearInterval(myInterval) : null;
 };
 
 // function to run infinitely thru names
 const start = () => {
-  if (myInterval) {
-    clearInterval(myInterval);
-    myInterval = 0;
-    soundEls[0].pause();
-    // soundEl.current.pause();
-    return;
-  }
-//   soundEl.current.play();
-  generateRandomName();
+    if (myInterval) {
+        clearInterval(myInterval);
+        myInterval = 0;
+        soundEls[0].pause();
+        // soundEl.current.pause();
+        return;
+    }
+    //   soundEl.current.play();
+    generateRandomName();
 };
 
 // Function creating list item as random name is generated
@@ -35,12 +36,11 @@ const generateRandomName = () => {
         clearBox();
         soundEls[0].play();
         let randomName = names[Math.floor(Math.random() * names.length)];
-        let listItem = document.createElement("li");
-        listItem.id = 'randomName'
-        listItem.innerHTML = randomName // Delivers a random name
+        let listItem = document.createElement('li');
+        listItem.id = 'randomName';
+        listItem.innerHTML = randomName; // Delivers a random name
         namesList.appendChild(listItem);
     }, 150);
-
 };
 
 getNameButton.onclick = start;
